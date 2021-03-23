5681a9d8249a3900c51c7a9a652ba7849343078c
Attila <kasimir_vda@icloud.com>
2021-03-23 15:19:52 +0100

    finished challenge wtf

--- ExerciseDay2/src/app/main-dyn/main-dyn.component.ts
+++ ExerciseDay2/src/app/main-dyn/main-dyn.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main-dyn',
@@ -6,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-dyn.component.css']
 })
 export class MainDynComponent implements OnInit {
+  // You should in this project, have an array of objects with cars and show them on the main page.
+  inputCars = new FormGroup({
+    name: new FormControl("", Validators.required),
+    price: new FormControl("", Validators.required),
+    picture: new FormControl("", Validators.required)
+    })
 
+  cars = [
+    {
+      name: "BMW",
+      price: "32.000",
+      picture: "https://wallpaperaccess.com/full/681489.jpg"
+    },{
+      name: "Tesla",
+      price: "45.000",
+      picture: "https://wallpaperaccess.com/full/681489.jpg"
+    },{
+      name: "BENZER",
+      price: "26.800",
+      picture: "https://wallpaperaccess.com/full/681489.jpg"
+    },{
+      name: "Aston Martin",
+      price: "99.009",
+      picture: "https://wallpaperaccess.com/full/681489.jpg"
+    }
+  ];
+
+  submitPush() {
+    if (this.inputCars.valid) {
+    let anyCar = this.inputCars.value;
+    console.table(anyCar);
+    this.cars.push(anyCar);
+    }
+}
   constructor() { }
-
   ngOnInit(): void {
   }
 
